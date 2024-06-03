840ee76380a50dbc8dc771b752dcc290adc61492
YuYuChia <t1423314243@gmail.com>
2024-06-03 13:34:09 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "JSONCodable",
     platforms: [
-        .macOS(.v10_14), .iOS(.v14)
+        .macOS(.v10_12), .iOS(.v14)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -20,12 +20,11 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "JSONCodable",
- 	        path: "JSONCodable/General"),
-    ],
-    swiftLanguageVersions: [.v5]
+            exclude: ["README.md"],
+            resources: [
+            ]
+        ),
+    ]
 )
